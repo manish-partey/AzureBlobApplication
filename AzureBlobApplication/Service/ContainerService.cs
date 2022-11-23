fc12849880b88ea5184f55fd7d8195ca2be4a294
manish-partey <manishk.partey@gmail.com>
2022-11-23 15:54:27 +0000

    Added container service class

---
+++ AzureBlobApplication/Service/ContainerService.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlobApplication.Service
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public ContainerService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+        public async Task CreateContainer(string containername)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containername);
+            await blobContainerClient.CreateIfNotExistsAsync();
+        }
+
+        public async Task DeleteContainer(string containername)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containername);
+            await blobContainerClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<List<string>> GetAllContainer()
+        {
+            List<string> containerNames = new();
+
+            await foreach (BlobContainerItem blobContainerItem in _blobServiceClient.GetBlobContainersAsync())
+            {
+                containerNames.Add(blobContainerItem.Name);
+            }
+            return containerNames;
+        }
+
+        public Task<List<string>> GetAllContaineraAndBlob()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AzureBlobApplication/Service/IContainerService.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureBlobApplication.Service
+{
+    public interface IContainerService
+    {
+        Task<List<string>> GetAllContaineraAndBlob();
+        Task<List<string>> GetAllContainer();        
+        Task CreateContainer(string containername);
+        Task DeleteContainer(string containername);
+    }
+}
