faa32cab7fcd982b9bbadcdb8eb111c7fded50b0
manish-partey <manishk.partey@gmail.com>
2022-11-23 14:17:10 +0000

    Add project files.

---
+++ AzureBlobApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureBlobApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
